a54ac2e6b42ef0af47fa227e5a36321f2e268e61
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2021-12-11 03:48:39 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,9 +1,6 @@
-//
-// Created by Kenneth Haga on 11/12/21.
-//
-
 #ifndef ESERCITAZIONE_9_LIB_H
 #define ESERCITAZIONE_9_LIB_H
+
 bool ricerca (char lista [10][20], char nome [1][20], int &pos);
 
 #endif //ESERCITAZIONE_9_LIB_H
