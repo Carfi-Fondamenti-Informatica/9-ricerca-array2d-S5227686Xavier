c36fbeabaa2c1856dc1ad5c93e944b98e649339a
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2021-12-11 13:06:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,13 @@
 #include "lib.h"
 #include <cstring>
 
-
-bool ricerca (char lista [10][20], char nome [1][20], int &pos){
+bool ricerca(char lista [10][20], char nome [1][20], int &pos){
     
-    for (int i = 0; i < 10; i++){
-        if (strcmp (lista[1], nome[0]) == 0){
-            pos = i;
+    for(int i=0; i<10; i++){
+        if (strcmp (lista [1], nome [0]) == 0){
+            pos =i;
             return true;
         }
     }
     return false;
 }
-
-
